7824facb80d0f1417489ef822692f865ff9f0265
Hart Liddell <hart@leapingfish.io>
2016-05-11 18:47:31 -0400

    Add missing open bracket and add better variable names

--- ga-tracking.js
+++ ga-tracking.js
@@ -1,50 +1,53 @@
-var gaTracker = (function(dLayer){
+var gaTracker = (function(dLayer) {
 
 	// Push the object to the dataLayer
 	// Parameters:
 	// eventName, category, action, label, Type String
 	// value Type Number (int)
-	var trackEvent = function(eventName, category, action, label, value) 
+	var pushEventToDataLayer = function(eventName, category, action, label, value) {
 		dLayer.push({
-			"event" 	: eventName,			// on GTM this would be the event that triggers the tag.
-			"category" 	: category,				
-			"action"	: action,
-			"label"		: label,
-			"value"		: value 
-		});	
+			"event": eventName, // on GTM this would be the event that triggers the tag.
+			"category": category,
+			"action": action,
+			"label": label,
+			"value": value
+		});
 	};
+
+	var isUniversalAnalytics = function(ga) {
+		return typeof ga !== "undefined" && typeof ga.getAll === 'function' ? true : false;
+	}
+
+	var isClassicAnalytics = function(_gat) {
+		return typeof _gat !== "undefined" && typeof _gat._getTrackers === 'function' ? true : false;
+	}
 
 	// decorate target URL for cross domain tracker.
 	var decorateLink = function(url) {
 		var trackers, linker;
 
-		//decorate the url
-		//if universal analytics is used
-		if(typeof ga !== "undefined" && typeof ga.getAll === 'function') {
+		if (isUniversalAnalytics(ga)) {
 			trackers = ga.getAll();
-			if(trackers.length) {
+			if (trackers.length) {
 				linker = new window.gaplugins.Linker(trackers[0]);
 				url = linker.decorate(url);
-			} 
+			}
 		}
 
-		//if classic analytics is used
-		if( typeof _gat !== "undefined" && typeof _gat._getTrackers === 'function') {
+		if (isClassicAnalytics(_gat)) {
 			trackers = _gat._getTrackers();
-			if(trackers.length) {
+			if (trackers.length) {
 				url = trackers[0]._getLinkerUrl(url);
 			}
-		} 
+		}
 
 		return url;
 
 	};
 
-	var tracker = {
-		"event" : trackEvent,
-		"decorateLink" : decorateLink
+	return {
+		"event": pushEventToDataLayer,
+		"decorateLink": decorateLink
 	};
-
-	return tracker;
 
 })(dataLayer);
